1bf25073a6bf0636cd51ad7d32b30fc43cc7c876
The Farrier <nkfarrey@gmail.com>
2020-06-16 22:22:32 -0500

    moved product schema to initialize first

--- models/stores.js
+++ models/stores.js
@@ -2,17 +2,17 @@ const User = require("./users")
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const productSchema = new Schema({
+  name: {type: String},
+  description: {type: String},
+  price: {type:Number},
+  quantity:{type:Number}
+})
+
 const storeSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: User}, //reference to user ID from user table
   store:[productSchema]
 });
-
-const productSchema = new Schema({
-  name: {type: String},
-  description: {type: String},
-  price: {tpye:Number},
-  quantity:{type:Number}
-})
 
 const Store = mongoose.model("Store",storeSchema)
 
