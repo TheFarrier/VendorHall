f46fda7310f6088f40470e48631030b278c9ac0b
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-06-22 18:29:40 -0500

    added Breadcrumb and InputTag components

---
+++ client/src/components/Productcard/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Productcard from './productcard';
+
+
+export default class Productcard extends React.Component {
+    render() {
+        return (
+            <div>
+                <Productcard />
+            </div>
+        )
+    }
+}
