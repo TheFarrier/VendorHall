035b0c5786a178872c3bd74db4e31cf192ab972e
The Farrier <nkfarrey@gmail.com>
2020-06-27 12:33:52 -0500

    lint

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
-const router = require("express").Router();
-const productRoutes = require("./products");
-const userRoutes = require("./users");
+const router = require('express').Router();
+const productRoutes = require('./products');
+const userRoutes = require('./users');
 
 // product routes
-router.use("/products", productRoutes);
-router.use("/user", userRoutes);
+router.use('/products', productRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,16 +1,16 @@
-const router = require("express").Router();
-const productController = require("../../controllers/productController");
+const router = require('express').Router();
+const productController = require('../../controllers/productController');
 
 // Matches with "/api/products"
-router.route("/")
+router.route('/')
   .get(productController.findAllProducts)
   .post(productController.createProduct);
 
 // Matches with "/api/products/:id"
-router.route("/:id")
+router.route('/:id')
   .get(productController.findProduct)
   .put(productController.updateProduct)
   .delete(productController.removeProduct);
 
-  
+
 module.exports = router;
