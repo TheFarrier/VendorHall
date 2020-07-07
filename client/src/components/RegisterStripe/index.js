6edaf52005c5c3ab1494a41074c8ff41f53f9c15
The Farrier <nkfarrey@gmail.com>
2020-07-06 21:56:12 -0500

    stripe form fills in user id

--- client/src/components/RegisterStripe/index.js
+++ client/src/components/RegisterStripe/index.js
@@ -2,25 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import './style.css';
 import API from '../../utils/API'
+import { useAuth0 } from '../../react-auth0-spa';
 
 
 
 function RegisterStripe () {
 
   const [stripe, setStripe] = useState({});
+  const { loading, user } = useAuth0();
 
   useEffect(()=>{
-    API.getStripeInfo()
-      .then((res)=>{
-        console.log(res)
-        setStripe(res.data)
-      })
-      .catch((err) => console.log(err));
-  },[])
+      API.getStripeInfo()
+        .then((res)=>{
+          setStripe(res.data)
+        })
+        .catch((err) => console.log(err));
+    },[])
+  
 
 
 
-  let stripelink = `https://connect.stripe.com/express/oauth/authorize?client_id=${stripe.client_id}&state=${stripe.stateValue}&stripe_user[country]=US&suggested_capabilities[]=transfers`
+  let stripelink = `https://connect.stripe.com/express/oauth/authorize?client_id=${stripe.client_id}&state=${stripe.stateValue}&stripe_user[country]=US&suggested_capabilities[]=transfers&stripe_user[email]=${user.email}`
 
   return (
     <div>
