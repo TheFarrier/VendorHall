e377309a664cdddc10ba7bcb59555af4136e82e7
The Farrier <nkfarrey@gmail.com>
2020-07-08 18:19:16 -0500

    product card now has key

--- client/src/components/ShoppingCart/CartCard.js
+++ client/src/components/ShoppingCart/CartCard.js
@@ -8,21 +8,23 @@ function ShoppingCart({ p, i }) {
   
   return (
 
-    <Container>
-      <Row>
-        <Col lg={3}>
-          <Card.Img className="cart-image" variant="left" src={p.image}></Card.Img>
-        </Col>
-        <Col lg={3}>
-          <Card.Body className="cart-title">{p.name}</Card.Body>
-        </Col>
-        <Col lg={1}>
-          <Card.Body className="cart-qty">1</Card.Body>
-        </Col>
-        <Col lg={3}>
-          <Card.Body className="cart-price">${p.price}</Card.Body>
-        </Col>
-      </Row>
+    <Container key={i}>
+      <Link to={`/product/${p.listID}`}>
+        <Row>
+          <Col lg={3}>
+            <Card.Img className="cart-image" variant="left" src={p.image}></Card.Img>
+          </Col>
+          <Col lg={3}>
+            <Card.Body className="cart-title">{p.name}</Card.Body>
+          </Col>
+          <Col lg={1}>
+            <Card.Body className="cart-qty">1</Card.Body>
+          </Col>
+          <Col lg={3}>
+            <Card.Body className="cart-price">${p.price}</Card.Body>
+          </Col>
+        </Row>
+      </Link>
     </Container>
   );
 }
