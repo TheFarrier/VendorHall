73bb4162cbcdd38d695cff022c0063d6f39daf7f
The Farrier <nkfarrey@gmail.com>
2020-07-13 04:12:04 -0500

    Created test search for both name & description

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,6 +14,15 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  searchProducts: function(req, res) {
+    db.Products
+      .find({$or:[
+        {name:{$regex: req.body, $options: 'i'}},
+        {description:{$regex: req.body, $options: 'i'}}
+      ]})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   createProduct: function(req, res) {
     db.Products
       .create(req.body)
@@ -32,5 +41,6 @@ module.exports = {
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  },
+
 };
